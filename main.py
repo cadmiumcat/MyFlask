794829971e76c862b1ce08eb453d7fa8a03f0371
catalina <catalina.garcia.mail@gmail.com>
2019-07-04 11:10:50 +0100

    Uses template (bootstrap/base.html)

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
-    return "Go away"
+    return render_template("base.html")
 
 
 @app.route("/user")
